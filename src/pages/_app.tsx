a9504c6e67183bf26a0870a2da44da24ea91693f
Ícaro Harry <icaro.harry@betrybe.com>
2021-04-03 15:30:16 -0300

    :lipstick: Define global styles

---
+++ src/pages/_app.tsx
@@ -0,0 +1,21 @@
+import type { AppProps } from "next/app";
+import Head from "next/head";
+
+import GlobalStyles from "styles/global";
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>Next Boilerplate</title>
+        <link rel="shortcut icon" href="/img/icon-512.png" />
+        <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <meta name="description" content="Next boilerplate" />
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default App;
